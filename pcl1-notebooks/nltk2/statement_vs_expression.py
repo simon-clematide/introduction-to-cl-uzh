0ba5fbabd7e729fcd5959411c6d9df56c1e3067d
Simon Clematide <simon.clematide@uzh.ch>
2025-12-19 01:30:58 +0100

    Add statement_vs_expression.py to illustrate statements and expressions in Python

---
+++ pcl1-notebooks/nltk2/statement_vs_expression.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# simon.clematide@uzh.ch
+# PCL I
+l = [1,2,3]
+# Simple statement
+del l[0]
+
+# If statement with complex expressions in it
+if len("A "+"String") > 5:
+    print("A "+"String".lower())
+
+# Principle
+# Statements are not expressions
+
+# Question
+# But why can I write interactively?
+
+"A Statement"
+
+# or
+
+3*4+8
